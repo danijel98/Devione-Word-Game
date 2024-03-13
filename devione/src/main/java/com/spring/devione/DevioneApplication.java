1dcc0a59a09840b8348578d0f16b70ff99285ae5
danijel98 <danijel_vuk@hotmail.com>
2024-03-13 15:44:20 +0100

    Initial commit

---
+++ devione/src/main/java/com/spring/devione/DevioneApplication.java
@@ -0,0 +1,13 @@
+package com.spring.devione;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevioneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevioneApplication.class, args);
+	}
+
+}
---
+++ devione/src/test/java/com/spring/devione/DevioneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.devione;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevioneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
